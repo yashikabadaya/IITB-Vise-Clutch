2332dce72f18b3a6cf790df0dc31e8b4fe0dc372
ybadaya <badaya.yashika@learner.manipal.edu>
2017-12-06 23:56:11 +0530

    to be tested

---
+++ movements/movements.ino
@@ -0,0 +1,93 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(0,1)
+int enableA = 3;
+int MotorA1 = 4;
+int MotorA2 = 7;
+
+int enableB = 10;
+int MotorB1 = 8;
+int MotorB2 = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin (9600); //configure pin modes
+//pinMode (pwm, OUTPUT);
+pinMode (enableA, OUTPUT);
+pinMode (MotorA1, OUTPUT);
+pinMode (MotorA2, OUTPUT);
+pinMode (enableB, OUTPUT);
+pinMode (MotorB1, OUTPUT);
+pinMode (MotorB2, OUTPUT);
+
+}
+
+void loop() {
+  
+//move fwd
+
+if(mySerial.available()=='11') {
+analogWrite(enableA, 255);
+analogWrite(enableB, 255);
+
+digitalWrite (MotorA1, LOW);
+digitalWrite (MotorA2, HIGH);
+
+
+digitalWrite (MotorB1, LOW);
+digitalWrite (MotorB2, HIGH);
+Serial.println("Moving fwd :)")
+
+
+//move back
+
+if(mySerial.available()=='00') {
+analogWrite(enableA, 255);
+analogWrite(enableB, 255);
+
+digitalWrite (MotorA1, HIGH);
+digitalWrite (MotorA2, LOW);
+
+
+digitalWrite (MotorB1, HIGH);
+digitalWrite (MotorB2, LOW);
+Serial.println("Moving backwards :(")
+
+
+
+//turn right
+
+if(mySerial.available()=='01') {
+analogWrite(enableA, 125);
+analogWrite(enableB, 255);
+
+digitalWrite (MotorA1, LOW);
+digitalWrite (MotorA2, HIGH);
+
+
+digitalWrite (MotorB1, LOW);
+digitalWrite (MotorB2, HIGH);
+Serial.println("Turning right :)")
+
+
+
+//turn left
+
+if(mySerial.available()=='10') {
+analogWrite(enableA, 255);
+analogWrite(enableB, 125);
+
+digitalWrite (MotorA1, LOW);
+digitalWrite (MotorA2, HIGH);
+
+
+digitalWrite (MotorB1, LOW);
+digitalWrite (MotorB2, HIGH);
+Serial.println("Turning left :)")
+
+
+
+}
+
+
+}
